4f2b7f2f6fb7f945c6e9f20d5943f3b2144822e1
wonder209 <stafftech66@gmail.com>
2026-02-08 02:16:53 +0300

    Render port fix

--- app.py
+++ app.py
@@ -272,5 +272,7 @@ def download():
         progress_dict[dl_id] = {"percent": 0}
         return f"Error: {str(e)}", 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
